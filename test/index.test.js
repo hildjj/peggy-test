e27bd58334bc734737a912297dc0496c65e14494
Joe Hildebrand <joe-github@cursive.net>
2022-06-27 09:12:32 -0500

    Fix tests, add quiet mode

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,7 @@ import tap from "libtap";
 import url from "url";
 
 const fs = fsb.promises;
+const node = process.execPath;
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -37,7 +38,7 @@ async function metaTest() {
   });
 
   const testFile = path.join(dir, "foo-g.test.md");
-  await tap.spawn(process.argv0, [
+  await tap.spawn(node, [
     "bin/peggy-test.js",
     "-g",
     path.join(dir, "foo.peggy"),
@@ -47,44 +48,45 @@ async function metaTest() {
   const testTxt = await fs.readFile(testFile, "utf8");
   tap.match(testTxt, /^---/, "wrote test file");
 
-  await tap.spawn(process.argv0, [
+  await tap.spawn(node, [
     "bin/peggy-test.js",
     "test/test_md.test.md",
   ]);
 
-  await tap.spawn(process.argv0, [
+  await tap.spawn(node, [
     "bin/peggy-test.js",
     "examples/foo.test.md",
   ]);
 
-  await tap.spawn(process.argv0, [
+  await tap.spawn(node, [
     "bin/peggy-test.js",
     "test/todo.test.md",
   ]);
 
-  await tap.spawn(process.argv0, [
+  await tap.spawn(node, [
     "bin/peggy-test.js",
+    "-q",
     "test/error.test.md",
   ], { expectFail: true });
 
-  await tap.spawn(process.argv0, [
+  await tap.spawn(node, [
     "bin/peggy-test.js",
     "test/noSource.test.md",
   ], { expectFail: true });
 
-  await tap.spawn(process.argv0, [
+  await tap.spawn(node, [
     "bin/peggy-test.js",
     "test/badInput.test.md",
   ]);
 
-  await tap.spawn(process.argv0, [
+  await tap.spawn(node, [
     "bin/peggy-test.js",
     "-g",
     "test/badGenerate.peggy",
     "test/badGenerate.test.md",
   ]);
 
-  await tap.spawn(process.argv0, [
+  await tap.spawn(node, [
     "bin/peggy-test.js",
     "-g",
     path.join(dir, "foo.peggy"),
@@ -92,7 +94,7 @@ async function metaTest() {
   ], { expectFail: true });
 
   const updateFile = path.join(dir, "foo.test.md");
-  await tap.spawn(process.argv0, [
+  await tap.spawn(node, [
     "bin/peggy-test.js",
     "-u",
     updateFile,
